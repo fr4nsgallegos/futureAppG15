29d251eaec8743f2e4a62966a13eec7632b76f21
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2026-05-28 20:37:55 -0500

    error con funci+on

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -22,29 +22,52 @@ class HomePage extends StatelessWidget {
     // return "estos son lo datos";
   }
 
+  // Otro ejemplo con error
+  Future<int> dividir(int a, int b) async {
+    print("Analizando división");
+    if (b == 0) {
+      throw Exception("No se puede dividir entre cero");
+    }
+    await Future.delayed(Duration(seconds: 2));
+    return a ~/ b;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       floatingActionButton: FloatingActionButton(
         onPressed: () async {
+          // Ejemplo usando dividir()
           try {
-            print("Cargando....");
-            final datos = await obtenerDatos();
-            print(datos);
-          } catch (error) {
-            print("Ocurríó un error: $error");
+            final int resultado = await dividir(10, 0);
+            print("el resultado de la división es: $resultado");
+          } catch (e) {
+            print("Hubo un error: $e");
           }
 
+          // ----------------------------------------------------------------------------------
+          // Ejemplo usando obtenerDatos()
+          // try {
+          //   print("Cargando....");
+          //   final datos = await obtenerDatos();
+          //   print(datos);
+          // } catch (error) {
+          //   print("Ocurríó un error: $error");
+          // }
+
+          // ----------------------------------------------------------------------------------
           //  Esto es incorrecto cuando devuelve un error
           // obtenerDatos().then((elDato) {
           //   print(elDato);
           // });
 
+          // ----------------------------------------------------------------------------------
           // // Manera 1 de acceder al contenido de un future
           // tarea1().then((nombre) {
           //   print(nombre);
           // });
 
+          // ----------------------------------------------------------------------------------
           // // Manera 2 de acceder al contenido de un future
           // String nombre = await obtenerNombre();
           // print(nombre);
